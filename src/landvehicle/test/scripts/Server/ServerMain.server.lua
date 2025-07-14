63c51b24655ce20f4eec244e7d54713577f56cca
Jason Yi <jasonyi1413@gmail.com>
2025-07-15 04:08:18 +1000

    fix: clean up missing plural changes

--- src/landvehicle/test/scripts/Server/ServerMain.server.lua
+++ src/landvehicle/test/scripts/Server/ServerMain.server.lua
@@ -4,9 +4,9 @@
 local ServerScriptService = game:GetService("ServerScriptService")
 
 local loader = ServerScriptService:FindFirstChild("LoaderUtils", true).Parent
-local require = require(loader).bootstrapGame(ServerScriptService.landvehicles)
+local require = require(loader).bootstrapGame(ServerScriptService.landvehicle)
 
 local serviceBag = require("ServiceBag").new()
-serviceBag:GetService(require("LandvehiclesService"))
+serviceBag:GetService(require("LandvehicleService"))
 serviceBag:Init()
 serviceBag:Start()
--- test/scripts/Client/ClientMain.client.lua
+++ test/scripts/Client/ClientMain.client.lua
@@ -1,10 +1,10 @@
 --[[
 	@class ClientMain
 ]]
-local loader = game:GetService("ReplicatedStorage"):WaitForChild("landvehicles"):WaitForChild("loader")
+local loader = game:GetService("ReplicatedStorage"):WaitForChild("landvehicle"):WaitForChild("loader")
 local require = require(loader).bootstrapGame(loader.Parent)
 
 local serviceBag = require("ServiceBag").new()
-serviceBag:GetService(require("LandvehiclesServiceClient"))
+serviceBag:GetService(require("LandvehicleServiceClient"))
 serviceBag:Init()
 serviceBag:Start()
